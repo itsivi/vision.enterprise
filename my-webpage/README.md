85227a3ac9a72e05b4927871edb436cd13280b26
itsivi <iniko001@odu.edu>
2024-07-07 16:36:44 +0300

    Add files via upload

---
+++ my-webpage/README.md
@@ -0,0 +1,4 @@
+# My webpage
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/its_ivi9/pen/VwJwLRK](https://codepen.io/its_ivi9/pen/VwJwLRK).
+
---
+++ my-webpage/dist/script.js
@@ -0,0 +1,16 @@
+function showSection(sectionId) {
+    // Hide all sections
+    const sections = document.querySelectorAll('.content-section');
+    sections.forEach(section => {
+        section.classList.remove('active');
+    });
+
+    // Show the selected section
+    const activeSection = document.getElementById(sectionId);
+    activeSection.classList.add('active');
+}
+
+// Show the first section by default
+document.addEventListener('DOMContentLoaded', () => {
+    showSection('about');
+});
